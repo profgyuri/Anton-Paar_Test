1d0f8b55b9d90d252614d575d906c95543d1d437
Turbék György <gyorgy.turbek@gmail.com>
2022-01-10 19:21:15 +0100

    Add project files.

---
+++ WordCounter.Lib/ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿namespace WordCounter.Lib.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+using System.Runtime.CompilerServices;
+    using System.Text;
+
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
