9e469ecf52f76d9b988a99e48c7358c1b3339eef
Turbék György <gyorgy.turbek@gmail.com>
2022-01-13 22:08:49 +0100

    Added drag 'n drop feature to textbox.

--- WordCounter/MainWindow.xaml.cs
+++ WordCounter/MainWindow.xaml.cs
@@ -3,10 +3,11 @@
     using System.Windows;
     using System.Windows.Forms;
     using WordCounter.Lib.ViewModels;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,6 +26,24 @@
                 var viewModel = (MainWindowViewModel)this.DataContext;
                 viewModel.FilePath = dialog.FileName;
             }
+        }
+
+        private void PathTextBox_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            var data = e.Data as System.Windows.DataObject;
+
+            if (data.ContainsFileDropList())
+            {
+                var files = data.GetFileDropList();
+                var path = files[0];
+                var viewModel = (MainWindowViewModel)this.DataContext;
+                viewModel.FilePath = path;
+            }
+        }
+
+        private void PathTextBox_PreviewDragOver(object sender, System.Windows.DragEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
